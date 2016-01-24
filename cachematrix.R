45753b7d708a569748fd681dc87fcb6981593066
Sebastián <sebastiancerminati@gmail.com>
2016-01-24 18:52:48 -0300

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,6 @@
 ## Put comments here that give an overall description of what your
 ## functions do
-
 ## Write a short comment describing this function
-
 makeCacheMatrix <- function(x = matrix()) {
         inv <- NULL
         set <- function(y) {
@@ -14,8 +12,6 @@ makeCacheMatrix <- function(x = matrix()) {
         getinverse <- function() inv
         list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
-
-
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
